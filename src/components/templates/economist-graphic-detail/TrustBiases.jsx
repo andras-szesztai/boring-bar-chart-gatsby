d33aaf94c5127b3449ec7e13757f853e6ecb1527
andras-szesztai <andras@newzoo.com>
2020-02-22 07:09:06 +0100

    added texts

--- src/components/templates/economist-graphic-detail/TrustBiases.jsx
+++ src/components/templates/economist-graphic-detail/TrustBiases.jsx
@@ -1,19 +1,36 @@
 import React from "react"
 import styled from "styled-components"
 
-import { FlexContainer, GridContainer } from "../../atoms"
+import { FlexContainer, GridContainer, Title } from "../../atoms"
 
 const MainContainer = styled(GridContainer)`
-
+  height: 650px;
+  width: 700px;
+  @media (max-width: 768px) {
+    width: 500px;
+  }
 `
 
+
+
 export default function TrustBiases({ data }) {
-
   return (
-    <FlexContainer fullScreen>
-      <GridContainer height="500px" width="400px" withBorder>
-
-      </GridContainer>
+    <FlexContainer height="750px" width="100vw">
+      <MainContainer withBorder rows="45px 1fr 40px">
+        <FlexContainer withBorder justify="flex-start">
+          <Title
+            fontWeight="semiBold"
+            fontSize={2}
+          >
+            Equity analysts are less likely to recommend stocks from countries
+            their nation is biased against
+          </Title>
+        </FlexContainer>
+        <FlexContainer withBorder>Chart</FlexContainer>
+        <FlexContainer withBorder justify="flex-start" fontColor="gray">
+        *How much people from one country trust people from another country, relative to the consensus and their general level of trust
+        </FlexContainer>
+      </MainContainer>
     </FlexContainer>
   )
 }
