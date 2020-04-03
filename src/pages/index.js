336ea6d12338beae04902b495d70e722d86362a5
andras-szesztai <andras@newzoo.com>
2020-04-03 11:34:32 +0200

    checking contentful response

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,60 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import { graphql } from "gatsby"
+import { Link } from "gatsby"
+
 import { FlexContainer } from "../components/atoms"
 
-export default function IndexPage() {
-    
+
+
+
+export default function IndexPage({data}) {
+  const { allContentfulVisualizationsLink: { edges: list } } = data
+  console.log(list);
+  
+//   <p>
+// Check out my <Link to="/blog">blog</Link>!
+// </p>
+// <p>
+// {/* Note that external links still use `a` tags. */}
+// Follow me on <a href="https://twitter.com/gatsbyjs">Twitter</a>!
+// </p>
   return (
     <>
       <Helmet title="Boring Bar Chart" />
       <FlexContainer
-        fontSize={10}
-        fontWeight={0}
+        // fontSize={10}
+        // fontWeight={0}
         fullScreen
+        direction="column"
       >
-        Coming soon
+      {
+        list && list.map(({ node: { title, link, isOutside } }) => <div>{title}</div>)
+      }
       </FlexContainer>
     </>
   )
 }
+
+
+export const query = graphql`
+  {
+    allContentfulVisualizationsLink(sort: {fields: date, order: DESC}) {
+      edges {
+        node {
+          date
+          title
+          id
+          description
+          link
+          isOutside
+          image {
+            fluid {
+              ...GatsbyContentfulFluid_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`
