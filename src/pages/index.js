add1d39247acb4181b8041fdbd64c890f6687a99
andras-szesztai <andras@newzoo.com>
2020-04-03 17:00:40 +0200

    styled for mobile portrait

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,21 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import { graphql } from "gatsby"
 import { Link } from "gatsby"
-import { isMobile } from "react-device-detect"
+import { isMobileOnly, withOrientationChange } from "react-device-detect"
 import Image from "gatsby-image"
-
+import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa"
 import styled from "styled-components"
 
-import { FlexContainer, GridContainer, LinkAnchor } from "../components/atoms"
+import {
+  FlexContainer,
+  GridContainer,
+  Container,
+} from "../components/atoms"
 import { themifyTransition, themifySpace, themifyColor } from "../themes/mixins"
+import { colors } from "../themes/theme"
 
 const Layout = styled(GridContainer)`
-  grid-template-rows: 50px 1fr;
+  grid-template-rows: 60px 1fr;
   grid-row-gap: 0;
   max-height: 100vh;
   padding-bottom: 2rem;
@@ -19,28 +24,34 @@ const Layout = styled(GridContainer)`
 
 const HeaderContainer = styled(FlexContainer)`
   width: 100vw;
-  height: 50px;
+  height: 60px;
   box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.3);
   z-index: 1000;
-  background-color: rgba(255, 255, 255, 0.8);
-  padding: 3rem;
+  padding: 3rem 3rem;
+
+  @media (min-width: 700px) {
+    padding: 3rem 4rem;
+  }
+  @media (min-width: 1400px) {
+    padding: 3rem 6rem;
+  }
 `
 
 const MainGrid = styled(GridContainer)`
-  padding: 3rem;
-  overflow-y: scroll;
+  padding: 4rem;
+  overflow-y: auto;
   grid-row-gap: 1rem;
 
   @media (min-width: 700px) {
     grid-template-columns: repeat(2, 2fr);
-    padding: 3rem;
-    grid-column-gap: 3rem;
+    padding: 6rem;
+    grid-column-gap: 4rem;
     grid-row-gap: 2rem;
   }
-  @media (min-width: 1400px) {
-    padding: 5rem;
+  @media (min-width: 1300px) {
+    padding: 8rem;
     grid-template-columns: repeat(3, 1fr);
-    grid-column-gap: 5rem;
+    grid-column-gap: 6rem;
     grid-row-gap: 3rem;
   }
 `
@@ -71,6 +82,7 @@ const SingleText = styled(FlexContainer)`
 `
 
 const LinkContainer = styled(FlexContainer)`
+  transition: all ${themifyTransition("sm")};
   background-color: transparent;
   padding: ${themifySpace(1)}px ${themifySpace(2)}px;
   border-radius: ${themifySpace(1)}px;
@@ -84,26 +96,48 @@ const LinkContainer = styled(FlexContainer)`
       color: ${themifyColor("grayDarkest")};
     }
   }
-
-  :hover {
-    a {
-      color: ${themifyColor("#000")};
-    }
-  }
 `
 
-export default function IndexPage({ data }) {
+function IndexPage({ data, isPortrait }) {
   const {
     allContentfulVisualizationsLink: { edges: list },
   } = data
-
+  const isSmallScreen = isPortrait && isMobileOnly
   return (
     <>
       <Helmet title="Boring Bar Chart" />
       <Layout>
         <HeaderContainer justify="space-between">
           <FlexContainer>Icon</FlexContainer>
-          <FlexContainer>Social links</FlexContainer>
+          <FlexContainer cursor="pointer">
+            <Container marginLeft={4} paddingTop={1}>
+              <a
+                href="https://twitter.com/AndSzesztai"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaTwitter color={colors.grayDarkest} size={25} />
+              </a>
+            </Container>
+            <Container marginLeft={4} paddingTop={1}>
+              <a
+                href="https://github.com/andras-szesztai"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub color={colors.grayDarkest} size={25} />
+              </a>
+            </Container>
+            <Container marginLeft={4} paddingTop={1}>
+              <a
+                href="https://www.linkedin.com/in/andr%C3%A1s-szesztai-351a4379/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin color={colors.grayDarkest} size={27} />
+              </a>
+            </Container>
+          </FlexContainer>
         </HeaderContainer>
         <MainGrid>
           {list.map(
@@ -111,7 +145,7 @@ export default function IndexPage({ data }) {
               <ItemContainer
                 pos="relative"
                 key={id}
-                height="200px"
+                height={isSmallScreen ? "150px" : "180px"}
                 borderRadius={1}
               >
                 <FlexContainer pos="relative" fullSize align="flex-start">
@@ -121,16 +155,16 @@ export default function IndexPage({ data }) {
                   absPos
                   fullSize
                   rows="min-content 1fr min-content"
-                  paddingLeft={3}
-                  paddingRight={3}
+                  paddingLeft={isSmallScreen ? 2 : 3}
+                  paddingRight={isSmallScreen ? 2 : 3}
                   paddingTop={2}
-                  paddingBottom={3}
-                  rowGap={2}
+                  paddingBottom={isSmallScreen ? 2 : 3}
+                  rowGap={isSmallScreen ? 1 : 2}
                 >
                   <SingleText
                     justify="flex-start"
                     fontWeight="medium"
-                    fontSize={3}
+                    fontSize={isSmallScreen ? 2 : 3}
                     lineHeight={1.25}
                   >
                     {title}
@@ -163,6 +197,8 @@ export default function IndexPage({ data }) {
   )
 }
 
+export default withOrientationChange(IndexPage)
+
 export const query = graphql`
   {
     allContentfulVisualizationsLink(sort: { fields: date, order: DESC }) {
@@ -184,34 +220,3 @@ export const query = graphql`
     }
   }
 `
-
-// {/* <HoverContainer fullSize pos="relative" cursor="pointer">
-//   {/* {!isOutside ? (
-//   <Link to={`${link}`}>{title}</Link>
-// ) : (
-//   <a href={`${link}`} target="_blank" rel="noopener noreferrer">
-//     {title}
-//   </a>
-// )} */}
-
-// <Image style={{ minWidth: "100%" }} fluid={image.fluid} />
-// </HoverContainer>
-// <TextContainer
-//   absPos
-//   fullSize
-//   rows="min-content 1fr"
-//   paddingLeft={2}
-//   paddingTop={1}
-// >
-//   <FlexContainer
-//     fontColor="#fff"
-//     justify="flex-start"
-//     fontWeight="medium"
-//     fontSize={3}
-//   >
-//     {title}
-//   </FlexContainer>
-//   <FlexContainer fontColor="#fff" justify="flex-start">
-//     {description}
-//   </FlexContainer>
-// </TextContainer>
