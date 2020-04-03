a60a6b1fdf962bc775da2159d1a229ccfd1c8b53
andras-szesztai <andras@newzoo.com>
2020-04-03 12:19:22 +0200

    added media queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,29 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import { graphql } from "gatsby"
 import { Link } from "gatsby"
+import Image from "gatsby-image"
 
-import { FlexContainer } from "../components/atoms"
+import styled from "styled-components"
+
+import { FlexContainer, GridContainer } from "../components/atoms"
+
+const MainGrid = styled(GridContainer)`
+  padding: 1rem;
+  grid-row-gap: 1rem;
+
+  @media (min-width: 700px) {
+    padding: 2rem;
+    grid-template-columns: repeat(2, 2fr);
+    grid-column-gap: 2rem;
+    grid-row-gap: 2rem;
+  }
+  @media (min-width: 1400px) {
+    padding: 3rem;
+    grid-template-columns: repeat(3, 1fr);
+    grid-column-gap: 3rem;
+    grid-row-gap: 3rem;
+  }
+`
 
 export default function IndexPage({ data }) {
   const {
@@ -13,20 +34,24 @@ export default function IndexPage({ data }) {
   return (
     <>
       <Helmet title="Boring Bar Chart" />
-      <FlexContainer
-        fullScreen
-        direction="column"
-      >
-        {list.map(({ node: { title, link, isOutside } }) =>
-          !isOutside ? (
-            <Link to={`${link}`}>{title}</Link>
-          ) : (
-            <a href={`${link}`} target="_blank" rel="noopener noreferrer">
-              {title}
-            </a>
-          )
-        )}
-      </FlexContainer>
+      <FlexContainer height="100px">Header</FlexContainer>
+      <MainGrid>
+        {list.map(({ node: { id, title, link, isOutside, image } }) => (
+          <FlexContainer key={id} withBorder height="200px">
+          <Image
+              style={{ width: 250, borderRadius: 2 }}
+              fluid={image.fluid}
+            />
+            {!isOutside ? (
+              <Link to={`${link}`}>{title}</Link>
+            ) : (
+              <a href={`${link}`} target="_blank" rel="noopener noreferrer">
+                {title}
+              </a>
+            )}
+          </FlexContainer>
+        ))}
+      </MainGrid>
     </>
   )
 }
