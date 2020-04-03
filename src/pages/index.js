05b0608dfa62918c082c241bb78c0b6efe3fce3f
andras-szesztai <andras@newzoo.com>
2020-04-03 11:47:19 +0200

    added some layout elements

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,41 +5,35 @@ import { Link } from "gatsby"
 
 import { FlexContainer } from "../components/atoms"
 
+export default function IndexPage({ data }) {
+  const {
+    allContentfulVisualizationsLink: { edges: list },
+  } = data
 
-
-
-export default function IndexPage({data}) {
-  const { allContentfulVisualizationsLink: { edges: list } } = data
-  console.log(list);
-  
-//   <p>
-// Check out my <Link to="/blog">blog</Link>!
-// </p>
-// <p>
-// {/* Note that external links still use `a` tags. */}
-// Follow me on <a href="https://twitter.com/gatsbyjs">Twitter</a>!
-// </p>
   return (
     <>
       <Helmet title="Boring Bar Chart" />
       <FlexContainer
-        // fontSize={10}
-        // fontWeight={0}
         fullScreen
         direction="column"
       >
-      {
-        list && list.map(({ node: { title, link, isOutside } }) => <div>{title}</div>)
-      }
+        {list.map(({ node: { title, link, isOutside } }) =>
+          !isOutside ? (
+            <Link to={`${link}`}>{title}</Link>
+          ) : (
+            <a href={`${link}`} target="_blank" rel="noopener noreferrer">
+              {title}
+            </a>
+          )
+        )}
       </FlexContainer>
     </>
   )
 }
 
-
 export const query = graphql`
   {
-    allContentfulVisualizationsLink(sort: {fields: date, order: DESC}) {
+    allContentfulVisualizationsLink(sort: { fields: date, order: DESC }) {
       edges {
         node {
           date
