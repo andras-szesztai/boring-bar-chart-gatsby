90deba8bcbe95451ae3be894b77d8f9893a80e22
andras-szesztai <andras@newzoo.com>
2020-05-17 12:05:10 +0200

    added content loader when person details card is closed

--- src/components/organisms/templateElemets/moviesDashboard/PersonDetailCard/PersonDetailCard.jsx
+++ src/components/organisms/templateElemets/moviesDashboard/PersonDetailCard/PersonDetailCard.jsx
@@ -177,21 +177,45 @@ export default function PersonDetailCard({
           <AnimatePresence>
             {loading || !isInitialized ? (
               <motion.div variants={OPACITY_VARIANT} {...ANIMATE_PROPS}>
-                <ContentLoader
-                  speed={2}
-                  width={400}
-                  height={200}
-                  viewBox="0 0 400 200"
-                  backgroundColor="#f3f3f3"
-                  foregroundColor="#c0c0c0"
-                  style={{
-                    position: "absolute",
-                  }}
-                >
-                  <rect x="5" y="6" rx="3" ry="3" width="255" height="36" />
-                  <rect x="5" y="45" rx="3" ry="3" width="255" height="150" />
-                  <rect x="264" y="6" rx="3" ry="3" width="132" height="189" />
-                </ContentLoader>
+                {isOpen ? (
+                  <ContentLoader
+                    speed={2}
+                    width={400}
+                    height={200}
+                    viewBox="0 0 400 200"
+                    backgroundColor="#f3f3f3"
+                    foregroundColor="#c0c0c0"
+                    style={{
+                      position: "absolute",
+                    }}
+                  >
+                    <rect x="5" y="6" rx="3" ry="3" width="255" height="36" />
+                    <rect x="5" y="45" rx="3" ry="3" width="255" height="150" />
+                    <rect
+                      x="264"
+                      y="6"
+                      rx="3"
+                      ry="3"
+                      width="132"
+                      height="189"
+                    />
+                  </ContentLoader>
+                ) : (
+                  <ContentLoader
+                    speed={2}
+                    width={400}
+                    height={50}
+                    viewBox="0 0 400 45"
+                    backgroundColor="#f3f3f3"
+                    foregroundColor="#c0c0c0"
+                    style={{
+                      position: "absolute",
+                      bottom: 4,
+                    }}
+                  >
+                    <rect x="190" y="0" rx="3" ry="3" width="205" height="42" />
+                  </ContentLoader>
+                )}
               </motion.div>
             ) : (
               <DetailCardContent variants={OPACITY_VARIANT} {...ANIMATE_PROPS}>
