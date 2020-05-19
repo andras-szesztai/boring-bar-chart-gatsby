656a12e5512c35cb63dc12884c7b33709e6cb06a
andras-szesztai <andras@newzoo.com>
2020-05-19 18:56:09 +0200

    added multifetch

--- src/pages/visualizations/coronavirus-hungary.js
+++ src/pages/visualizations/coronavirus-hungary.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react"
+import axios from "axios"
 
 import {
   DATA_URL_HU,
@@ -8,38 +9,35 @@ import { useFetchData } from "../../hooks"
 import { CoronaVirusHungaryDashboard } from "../../components/templates/"
 
 export default function() {
-  // const { isLoading, response } = useFetchData(DATA_URL_HU)
-  // const [enLoading, setEnLoading] = useState(false)
-  // const [enData, setEnData] = useState(undefined)
-  
-  // useEffect(() => {
-  //   if (response && !enLoading) {
-  //     const fetchData = async () => {
-  //       setEnLoading(true)
-  //       try {
-  //         const res = await fetch(DATA_URL_EN)
-  //         const json = await res.json()
-  //         setEnLoading(false)
-  //         setEnData(json)
-  //       } catch (error) {
-  //         console.log(error)
-  //       }
-  //     }
-  //     fetchData()
-  //   }
-  // }, [response, enData, enLoading])
-  
-  // console.log("isLoading", isLoading, enLoading)
-  // console.log("hUresponse", response)
-  // console.log("eNresponse", enData)
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState({
+    hun: undefined,
+    eng: undefined,
+  })
+
+  useEffect(() => {
+    setLoading(true)
+    axios
+      .all([axios.get(`${DATA_URL_HU}`), axios.get(`${DATA_URL_EN}`)])
+      .then(
+        axios.spread((hun, eng) => {
+          setLoading(false)
+          setData({ hun: hun.data, eng: eng.data })
+        })
+      )
+      .catch(function(error) {
+        console.log(error)
+      })
+  }, [])
+
+  console.log(data)
   return (
     <>
-      <div>Test</div>
-      {/* <CoronaVirusHungaryDashboard
-        loading={isLoading}
-        data={response}
-        enData={enData}
-      /> */}
+      <CoronaVirusHungaryDashboard
+        loading={loading}
+        data={data.hun}
+        enData={data.eng}
+      />
     </>
   )
 }
