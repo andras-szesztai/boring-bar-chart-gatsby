e28723d37a72cf29381f55e4efd91305bfed6fdc
andras-szesztai <andras@newzoo.com>
2020-06-07 09:02:29 +0200

    setting up TextContentGrid

--- src/components/organisms/templateElemets/moviesDashboard/charts/DateAxis/Tooltip/Tooltip.jsx
+++ src/components/organisms/templateElemets/moviesDashboard/charts/DateAxis/Tooltip/Tooltip.jsx
@@ -1,17 +1,24 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styled, { css } from "styled-components"
-import useResizeAware from 'react-resize-aware';
+import useResizeAware from "react-resize-aware"
+import capitalize from "lodash/capitalize"
 
 import { Image } from "../../.."
 import { dropShadow, space } from "../../../../../../../themes/theme"
+import {
+  themifyFontSize,
+  themifyFontWeight,
+} from "../../../../../../../themes/mixins"
+import { COLORS } from "../../../../../../../constants/moviesDashboard"
+import { useMeasure } from "react-use"
 
-const WIDTH = 240
+const WIDTH = 280
 const HEIGHT = 160
 const LINE_WIDTH = 16
 
 const TooltipContainer = styled.div`
   position: absolute;
-  height: ${HEIGHT}px;
+  width: ${WIDTH}px;
 
   ${({ left, top, bottom }) => css`
     left: ${left}px;
@@ -31,39 +38,55 @@ const TooltipContainer = styled.div`
   display: flex;
 `
 
+const ImageContainer = styled.div`
+  height: ${HEIGHT}px;
+`
+
 const TextContentGrid = styled.div`
   display: flex;
-  white-space: nowrap;
-  margin-left: ${space[2]}px;
+  flex-direction: column;
+  margin-left: 12px;
 
   .title {
+    font-size: ${themifyFontSize(1)};
+    font-weight: ${themifyFontWeight(4)};
+    color: ${COLORS.secondaryDark};
+  }
 
+  .date {
+    font-size: ${themifyFontSize(1)};
+    font-weight: ${themifyFontWeight(1)};
+    color: ${COLORS.textColor};
   }
 `
 
 export default function Tooltip({
   hoveredMovie: { id, data, xPosition, x, yPosition },
 }) {
-  const [resizeListener, sizes] = useResizeAware();
-  console.log("sizes", sizes)
-  // console.log("id", id)
-  // console.log("data", data)
-
-  // TODO: bottom or top 0
+  console.log("data", data)
   if (!id) return null
   return (
     <TooltipContainer
+      key={id}
       left={xPosition ? x - WIDTH - LINE_WIDTH : x + LINE_WIDTH * 2}
       top={0}
     >
-      {resizeListener}
-      <Image
-        url={data.poster_path}
-        height={HEIGHT - space[3]}
-        alt={`${data.title}-poster`}
-      />
+      <ImageContainer>
+        <Image
+          url={data.poster_path}
+          height={HEIGHT}
+          alt={`${data.title}-poster`}
+        />
+      </ImageContainer>
       <TextContentGrid>
-        <div>{data.title}</div>
+        <div className="title">{data.title}</div>
+        <div className="date">
+          Release year: {data.release_date.slice(0, 4)}
+        </div>
+        <div className="date">Media type: {capitalize(data.media_type)}</div>
+        <div className="date">
+          {data.character ? "Character" : "Job"}: {data.job.filter(Boolean).length ? data.job.join(", ") : "N/A"}
+        </div>
       </TextContentGrid>
     </TooltipContainer>
   )
